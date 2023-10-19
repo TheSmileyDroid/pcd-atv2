6e86df967afb40108f47b26e194e20970746b6b8
TheSmileyDroid <ghsilha@gmail.com>
2023-10-18 22:39:44 -0300

    a

--- item3/TrafficController.java
+++ item3/TrafficController.java
@@ -3,70 +3,63 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TrafficController {
-    Lock lock = new ReentrantLock();
-    Condition left = lock.newCondition();
-    Condition right = lock.newCondition();
-    int control = 0;
-    int carsOnBridge = 0;
+	Lock lock = new ReentrantLock();
+	Condition left = lock.newCondition();
+	Condition right = lock.newCondition();
+	int control = 0; // 0 = no control, 1 = right, -1 = left
+	int carsOnBridge = 0;
 
-    public void enterLeft() {
-        lock.lock();
-        try {
-            while (carsOnBridge >= 1 && control != -1) {
-                left.await();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            control = -1;
-            carsOnBridge++;
-            System.out.println("Left");
-            lock.unlock();
-        }
-    }
+	public void enterLeft() {
+		lock.lock();
+		try {
+			while (((control != -1) && carsOnBridge > 0)) {
+				left.await();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			carsOnBridge++;
+			System.out.println("Left");
+			lock.unlock();
+		}
+	}
 
-    public void enterRight() {
-        lock.lock();
-        try {
-            if (control != 1 && control != 0) {
-                right.await();
-            }
-            while (carsOnBridge >= 1 && control != 1) {
-                right.await();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            control = 1;
-            carsOnBridge++;
-            System.out.println("Right");
-            lock.unlock();
-        }
-    }
+	public void enterRight() {
+		lock.lock();
+		try {
+			while ((control != 1) && carsOnBridge > 0) {
+				right.await();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			carsOnBridge++;
+			System.out.println("Right");
+			lock.unlock();
+		}
+	}
 
-    public void leaveLeft() {
-        lock.lock();
-        try {
-            control = 0;
-            left.signal();
-            right.signal();
-        } finally {
-            System.out.println("Leave Left");
-            carsOnBridge--;
-            lock.unlock();
-        }
-    }
+	public void leaveLeft() {
+		lock.lock();
+		try {
+			carsOnBridge--;
+			System.out.println("Leave Left: " + carsOnBridge + " cars on bridge");
+			left.signal();
+			right.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
 
-    public void leaveRight() {
-        lock.lock();
-        try {
-            control = 0;
-            right.signal();
-            left.signal();
-        } finally {
-            System.out.println("Leave Right");
-            carsOnBridge--;
-            lock.unlock();
-        }
-    }
+	public void leaveRight() {
+		lock.lock();
+		try {
+			carsOnBridge--;
+			System.out.println("Leave Right: " + carsOnBridge + " cars on bridge");
+			right.signal();
+			left.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
 }
